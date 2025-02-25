28891d049d0f3632bcab2f6381fef4d6312f8e87
yuto-kimura-g <75498217+yuto-kimura-g@users.noreply.github.com>
2025-02-25 14:51:37 +0900

    update README
    succeeded to run cplex via docplex

--- README.md
+++ README.md
@@ -54,7 +54,7 @@ Mathematical Optimization with Python: sandbox
 |`mip`|`gurobi`|?|
 |---|---|---|
 |`pyscipopt`|`scip`|?|
-|`docplex`|`cplex`|x|
+|`docplex`|`cplex`|o *3|
 |`gurobipy`|`gurobi`|o *2|
 
 - o: 正常に動作した
@@ -62,3 +62,4 @@ Mathematical Optimization with Python: sandbox
 - ?: 未検証
 - *1: `we plan to support CPLEX/SCIP in the future` (https://github.com/coin-or/python-mip/blob/eb3dd63cf2e6dd632ec645997c2dc49e01af3e2b/mip/constants.py#L26, 2025/2/22閲覧)
 - *2: アカデミックライセンスでは未検証（pipで付いてきたサンプルで実行）
+- *3: pipの`cplex`が`<=python@3.10`な上に、uvだとexternally nmanagedと言ってくるので、brewでglobalに入れたpython@3.10で実行する必要がある
--- src/run-docplex.py
+++ src/run-docplex.py
@@ -1,22 +1,26 @@
 from docplex.mp.model import Model
 
 # Create a new model
-m = Model()
+# m = Model()
+with Model() as m:
 
-# Create variables
-x = m.continuous_var(name='x', lb=0, ub=1)
-y = m.continuous_var(name='y', lb=0, ub=1)
+    # Create variables
+    x = m.continuous_var(name='x', lb=0, ub=1)
+    y = m.continuous_var(name='y', lb=0, ub=1)
 
-# Add constraints
-m.add_constraint(x + y <= 1)
-m.add_constraint(x - y >= 1)
+    # Add constraints
+    m.add_constraint(x + y <= 1)
+    m.add_constraint(x - y >= 1)
 
-# Set the objective function
-m.maximize(x + y)
+    # Set the objective function
+    m.maximize(x + y)
 
-# Write the model to a file
-# m.export_as_lp('model.lp')
-# m.export_as_mps('model.mps')
+    # Write the model to a file
+    # m.export_as_lp('model.lp')
+    # m.export_as_mps('model.mps')
 
-# Solve the model
-m.solve()
+    # Solve the model
+    m.solve()
+
+    m.print_solution()
+    m.print_information()
